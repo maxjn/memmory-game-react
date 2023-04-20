ccadd670f371200b3f149c1a53e118c7e9c6adb1
max jn <maxjn763@gmail.com>
2023-04-20 12:07:20 +0430

    card style: - adding turns & cards variables - adding "shuffleCards" function for shuffling cards by clicking on "New Game" button - adding cards grid - adding card components with styles

---
+++ src/components/SingleCard.jsx
@@ -0,0 +1,13 @@
+import "./SingleCard.css";
+function SingleCard({ card }) {
+  return (
+    <div className="card">
+      <div>
+        <img className="front" src={card.src} alt="card front" />
+        <img className="back" src="/img/cover.png" alt="cover" />
+      </div>
+    </div>
+  );
+}
+
+export default SingleCard;
