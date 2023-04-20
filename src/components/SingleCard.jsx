70758d34aa28658cc35f0a52e7d57c39ad2bfb3b
Muhammad Haghnegahdar <104886566+maxjn@users.noreply.github.com>
2023-04-20 14:23:08 +0330

    Select Logic: (#2)
    - adding state for choices
    - adding "handle choices" function for handling choices
    - comparing choicen cards and increasing turn count
    - adding "matched" property to cards and setting this property to true after match

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,10 +1,15 @@
 import "./SingleCard.css";
-function SingleCard({ card }) {
+function SingleCard({ card, handleChoice }) {
   return (
     <div className="card">
       <div>
         <img className="front" src={card.src} alt="card front" />
-        <img className="back" src="/img/cover.png" alt="cover" />
+        <img
+          className="back"
+          src="/img/cover.png"
+          alt="cover"
+          onClick={() => handleChoice(card)}
+        />
       </div>
     </div>
   );
