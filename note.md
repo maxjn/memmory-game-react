023195bc67aacffa32e62e4922e3a14e886e888a
max jn <maxjn763@gmail.com>
2023-04-20 15:22:16 +0430

    Select Logic: - adding state for choices - adding "handle choices" function for handling choices - comparing choicen cards and increasing turn count - adding "matched" property to cards and setting this property to true after match

--- note.md
+++ note.md
@@ -4,3 +4,10 @@
 - adding "shuffleCards" function for shuffling cards by clicking on "New Game" button
 - adding cards grid
 - adding card components with styles
+
+# Choice logic
+
+- adding state for choices
+- adding "handle choices" function for handling choices
+- comparing choicen cards and increasing turn count
+- adding "matched" property to cards and setting this property to true after match
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  { src: "/img/helmet-1.png" },
-  { src: "/img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/sword-1.png" },
+  { src: "/img/helmet-1.png", match: false },
+  { src: "/img/potion-1.png", match: false },
+  { src: "/img/ring-1.png", match: false },
+  { src: "/img/scroll-1.png", match: false },
+  { src: "/img/shield-1.png", match: false },
+  { src: "/img/sword-1.png", match: false },
 ];
 
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
+  // Shuffling cards on new game
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
@@ -23,6 +26,38 @@ function App() {
     setTurns(0);
   };
 
+  // Handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  // Reset Choice
+  const resetChoice = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
+
+  // Comparing Choices
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src == choiceOne.src) {
+              return { ...card, match: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetChoice();
+      } else {
+        resetChoice();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -30,7 +65,7 @@ function App() {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
         ))}
       </div>
     </div>
